483a10f2c485ffb5aec2ddde759e9395992e0718
leegeunhyeok <lghlove0509@naver.com>
2018-03-21 15:58:54 +0900

    Write post, edit, delete and add admin

--- Python/FlaskTest/main.py
+++ Python/FlaskTest/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, json, request
+from flask import Flask, render_template, redirect, json, request, session
 import pymysql
+import datetime
 
 app = Flask(__name__)
 
@@ -11,9 +12,60 @@ cur = conn.cursor()
 def main():
     return render_template("index.html")
 
+@app.route("/logout")
+def logout():
+    try:
+        session.pop('user', None)
+        return '<script>alert("Logout success");location.href = "/"</script>'
+    except:
+        return '<script>location.href = "/"</script>'
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/process/loginCheck", methods=["POST"])
+def process_login_check():
+    try:
+        user = session['user']
+        return json.dumps({"result": user})
+    except:
+        return json.dumps({"result": False})
+
+@app.route("/process/getContent", methods=["POST"])
+def get_content():
+    cur.execute("SELECT * FROM content")
+    rows = cur.fetchall()
+    data = []
+    for row in rows:
+        data.append([row[0], row[1], row[3], row[4]])
+
+    return json.dumps({"data": data})
+
+@app.route("/process/getPost", methods=["POST"])
+def get_post():
+    _cid = request.form["cid"]
+    cur.execute("SELECT title, content, date FROM content WHERE _cid = %s", (_cid))
+    data = cur.fetchall()[0]
+    return json.dumps({"title":data[0], "content":data[1], "date":data[2]})
+
+@app.route("/process/deletePost", methods=["POST"])
+def delete_post():
+    _cid = request.form["cid"]
+    _id = request.form["id"]
+    print _cid, _id
+    try:
+        permission = session["user"]["permission"]
+        id = session["user"]["_id"]
+        print id
+        if permission or id == _id:
+            cur.execute("DELETE FROM content WHERE _cid = %s", (_cid))
+            conn.commit()
+            return json.dumps({"result": True, "message":"Done!"})
+        else:
+            return json.dumps({"result": False, "message":"Admin or %s can delete" % _id})
+    except:
+        return json.dumps({"result": False, "message":"Access denied."})
 
 @app.route("/process/login", methods=["POST"])
 def process_login():
@@ -27,6 +79,8 @@ def process_login():
     result = cur.execute("SELECT * FROM flask_user WHERE _id='%s' and password='%s'" % (_id, _password))
 
     if result > 0:
+        user = cur.fetchall()[0] # first user data
+        session['user'] = {"_id": user[0], "permission":user[2]} # Add user data at session
         return '<script>alert("Hello, %s");location.href = "/"</script>' % _id
     else:
         return '<script>alert("Access denied.");location.href = "/"</script>'
@@ -40,6 +94,10 @@ def process_signup():
     _id = request.form['id']
     _password = request.form['password']
     _password_re = request.form['password_re']
+    try:
+        _permission = request.form['permission'] and 1 or 0
+    except:
+        _permission = 0
 
     check_id = check(_id)
     check_password = check(_password)
@@ -57,7 +115,7 @@ def process_signup():
     result = cur.execute("SELECT * FROM flask_user WHERE _id='%s'" % _id)
     if result == 0:
         if _password == _password_re:
-            cur.execute("INSERT INTO flask_user VALUES (%s, %s)", (_id, _password))
+            cur.execute("INSERT INTO flask_user VALUES (%s, %s, %s)", (_id, _password, _permission))
             conn.commit()
             print "Join new user: %s %s" % (_id, _password)
             return "<script>alert('Hello new user, %s');location.href = '/login'</script>" % _id
@@ -66,6 +124,59 @@ def process_signup():
     else:
         print "%s is already exists" % _id
         return "<script>alert('This id is alreay exists');location.href = '/signup'</script>"
+
+# Write post
+@app.route("/process/write", methods=["POST"])
+def write():
+    try:
+        n = datetime.datetime.now()
+        _cid = n.strftime("%Y%M%S%f")
+        _id = session['user']['_id']
+        _title = request.form['title']
+        _content = request.form['content']
+        _date = n.strftime("%Y-%m-%d")
+        cur.execute("INSERT INTO content VALUES(%s, %s, %s, %s, %s)", (_cid, _title, _content, _date, _id))
+        conn.commit()
+        return "<script>alert('Done!');location.href = '/'</script>"
+    except:
+        return "<script>alert('Please login');location.href = '/login'</script>"
+
+# Edit post
+@app.route("/process/modifyPost", methods=["POST"])
+def modify():
+    try:
+        user = session['user']['_id']
+        title = request.form['title']
+        print title
+        content = request.form['content']
+        print content
+        id = request.form['id']
+        cid = request.form['cid']
+        print id, cid
+
+        if user == id:
+            cur.execute("UPDATE content SET title = %s, content = %s WHERE _cid = %s", (title, content, cid))
+            conn.commit()
+            return "<script>alert('Done!');location.href = '/'</script>"
+        else:
+            return "<script>alert('Only %s can edit');location.href = '/'</script>" % id
+    except:
+        return "<script>alert('Please login');location.href = '/login'</script>"
+
+@app.route("/user")
+def user():
+    try:
+        user = session['user']['permission']
+        if user == 1:
+            return render_template("user.html")
+        else:
+            return "<script>alert('Only admin!'); location.href='/'</script>"
+    except:
+        return "<script>alert('Please login'); location.href='/'</script>"
+
+@app.route("/connect")
+def connect():
+    return render_template("connect.html")
 
 def check(str):
     loop = 0
@@ -86,6 +197,7 @@ def check(str):
 
 if __name__ == "__main__":
     print "Starting at %s" % __name__
+    app.secret_key = "key_value_0509"
     app.run(threaded = True)
 else:
     print "Not main : %s" % __name__
---
+++ Python/FlaskTest/static/js/index.js
@@ -0,0 +1,111 @@
+var permission = 0;
+var login = 0;
+
+$(function() {
+    $.ajax({
+        url: '/process/loginCheck',
+        type: 'POST',
+        dataType: 'JSON',
+        success: function(data){
+            if(data.result){
+                $('#after-login').removeClass('hidden');
+                $('#before-login').addClass('hidden');
+                $('#user-id').text(data.result._id + ' 님');
+                permission = data.result.permission;
+                console.log('login');
+                login = 1;
+                if(permission) {
+                    alert('Hello, Admin');
+                }
+            } else {
+            login = 0;
+                $('#before-login').removeClass('hidden');
+                $('#after-login').addClass('hidden');
+                console.log('no-login');
+            }
+        }, error: function(err){
+            console.log(err.statusText);
+        }
+    });
+
+    var table = $('#content').DataTable({
+        "language": {
+            "lengthMenu": "_MENU_ 개씩 보기",
+            "zeroRecords": "작성된 글이 없습니다.",
+            "info": "_PAGES_중 _PAGE_ 페이지",
+            "infoEmpty": "",
+            "search": "검색",
+            "paginate": {
+                "previous": "이전",
+                "next": "다음"
+            }
+        },
+        "ajax": {
+            "url": "/process/getContent",
+            "type": "POST"
+         }
+    });
+
+    $('#content tbody').on('click', 'tr', function() {
+        var _cid = table.row(this).data()[0];
+        var _id = table.row(this).data()[3];
+
+        $.ajax({
+            url: '/process/getPost',
+            type: 'POST',
+            dataType: 'JSON',
+            data: {'cid': _cid},
+            success: function(data) {
+                $('#content-title').text(data.title);
+                $('#content-main').text(data.content);
+                $('#content-id').text(_cid);
+                $('#content-date').text(data.date);
+                $('#content-writer').text(_id);
+                $('#view-modal').modal();
+            }, error: function(err) {
+                console.log(err.statusText);
+            }
+        })
+    });
+
+    $('#delete-btn').click(function() {
+        $.ajax({
+            url: '/process/deletePost',
+            type: 'POST',
+            dataType: 'JSON',
+            data: {'cid': $('#content-id').text(), 'id':$('#content-writer').text()},
+            success: function(data) {
+                console.log(data.message);
+                if(data.result) {
+                    alert(data.message);
+                    location.href = '/';
+                } else {
+                    alert(data.message);
+                }
+            },
+            error: function(err) {
+                console.log(err.statusText);
+            }
+        })
+    });
+
+    $('#edit-btn').click(function() {
+        var oldTitle = $('#content-title').text();
+        var oldContent = $('#content-main').text();
+
+        $('#modify-title').val(oldTitle);
+        $('#modify-content').val(oldContent);
+        $('#modify-cid').val($('#content-id').text());
+        $('#modify-id').val($('#content-writer').text());
+
+        $('#modify-modal').modal();
+    });
+
+    $('#open-write-modal').click(function() {
+        if(login) {
+            $('#write-modal').modal();
+        } else {
+            alert('Access denied.');
+        }
+    })
+});
